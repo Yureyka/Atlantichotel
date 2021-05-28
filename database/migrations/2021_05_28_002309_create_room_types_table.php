084682b4834fbabc02907e39f7691825c7ba281f
Yuri <enotyay@gmail.com>
2021-05-28 03:40:00 +0300

    migrated to db

---
+++ database/migrations/2021_05_28_002309_create_room_types_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('room_types', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['Одноместный номер', 'Двухместный номер', 'Трехместный номер', 'Номер Люкс']);
+            $table->text('description');
+            $table->string('price');
+            $table->enum('city', ['Феодосия', 'Симферополь', 'Севастополь', 'Саки']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room_types');
+    }
+}
